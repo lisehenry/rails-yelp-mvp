6066c18723cf8772ff6b4a03fc4d37be9e1bbae6
lise henry <lise.henry@essca.eu>
2021-02-24 15:13:07 +0100

    added models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 6 }
+  validates :content, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# puts "Creating 5 fake restaurants."
+
+# 5.times do
+
+# puts "Finished!"
+
